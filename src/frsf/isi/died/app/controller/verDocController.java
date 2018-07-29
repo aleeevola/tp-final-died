3cf42cca293156056ba0788d37abf7517c39ef70
aleeevola <aleeevola@gmail.com>
2018-07-29 12:42:53 -0300

    paso 5 terminado

--- src/frsf/isi/died/app/controller/verDocController.java
+++ src/frsf/isi/died/app/controller/verDocController.java
@@ -1,11 +1,13 @@
 package frsf.isi.died.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import frsf.isi.died.app.dao.MaterialCapacitacionDao;
 import frsf.isi.died.app.dao.MaterialCapacitacionDaoDefault;
-import frsf.isi.died.app.vista.material.DeseosPanel;
-import frsf.isi.died.app.vista.material.LibroPanel;
 import frsf.isi.died.app.vista.material.verDocPanel;
-import frsf.isi.died.tp.modelo.productos.Libro;
+import frsf.isi.died.tp.estructuras.Nodo;
+import frsf.isi.died.tp.estructuras.TipoDeDato;
 import frsf.isi.died.tp.modelo.productos.MaterialCapacitacion;
 
 public class verDocController {
@@ -37,5 +39,29 @@ public class verDocController {
 		this.panelDeseos = panelDeseos;
 	}
 	
+	public void buscar(String titulo,String autor,String seccion,String capitulo,String editorial,String palabra,
+	String fecha) {
+		List<Nodo> resultados= new ArrayList<>();
+		for(Nodo n : materialDAO.listaDocumentos()) {
+			List<Nodo> lista=n.arbolEnPreorden();
+			if(
+			contiene(TipoDeDato.TITULO,titulo,lista) &&
+			contiene(TipoDeDato.AUTOR,autor,lista) &&
+			contiene(TipoDeDato.SECCION,seccion,lista) &&
+			contiene(TipoDeDato.CAPITULO,capitulo,lista) &&
+			contiene(TipoDeDato.EDITORIAL,editorial,lista) &&
+			contiene(TipoDeDato.PALABRA_CLAVE,palabra,lista) &&
+			contiene(TipoDeDato.FECHA_PUBLICACION,fecha,lista)
+			) resultados.add(n);
+		}
+		panelDeseos.setListaDocumento(resultados, true);
+	}
 	
+	public boolean contiene(TipoDeDato tipo,String txt, List<Nodo> lista) {
+		if(txt==null) return true;
+		for(Nodo n: lista) {
+			if(n.getTipoNodo()==tipo && n.getValor().contains(txt)) return true;
+		}
+		return false;
+	}
 }
--- src/frsf/isi/died/app/vista/material/verDocPanel.java
+++ src/frsf/isi/died/app/vista/material/verDocPanel.java
@@ -3,9 +3,14 @@ package frsf.isi.died.app.vista.material;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 
 import frsf.isi.died.app.controller.DocumentoController;
 import frsf.isi.died.app.controller.verDocController;
@@ -18,6 +23,30 @@ public class verDocPanel extends JPanel{
 	private JScrollPane scrollPane;
 	private JTable tabla;
 
+	private JLabel lblTitulo;
+//	private JLabel lblMetadato;
+	private JLabel lblAutor;
+	private JLabel lblSeccion;
+//	private JLabel lblParrafo;
+	private JLabel lblCapitulo;
+	private JLabel lblEditorial;
+//	private JLabel lblResumen;
+	private JLabel lblPalabra;
+	private JLabel lblFecha;
+	
+	private JTextField txtTitulo;
+//	private JTextField txtMetadato;
+	private JTextField txtAutor;
+	private JTextField txtSeccion;
+//	private JTextField txtParrafo;
+	private JTextField txtCapitulo;
+	private JTextField txtEditorial;
+//	private JTextField txtResumen;
+	private JTextField txtPalabra;
+	private JTextField txtFecha;
+	
+	private JButton btnFiltrar;
+	
 
 	private verDocTableModel tableModel;
 
@@ -30,14 +59,107 @@ public class verDocPanel extends JPanel{
 	
 	public void construir() {
 		GridBagConstraints gridConst= new GridBagConstraints();
+		
+		lblTitulo = new JLabel("Titulo: ");
+		gridConst.gridx=0;
+		gridConst.gridy=0;
+		this.add(lblTitulo, gridConst);
+		
+		lblAutor = new JLabel("Autor: ");
+		gridConst.gridx=0;
+		gridConst.gridy=1;
+		this.add(lblAutor, gridConst);
+		
+		lblSeccion = new JLabel("Seccion: ");
+		gridConst.gridx=0;
+		gridConst.gridy=2;
+		this.add(lblSeccion, gridConst);
+
+		lblCapitulo = new JLabel("Capitulo: ");
+		gridConst.gridx=0;
+		gridConst.gridy=3;
+		this.add(lblCapitulo, gridConst);
+		
+		lblEditorial = new JLabel("Editorial: ");
+		gridConst.gridx=0;
+		gridConst.gridy=4;
+		this.add(lblEditorial, gridConst);
+
+		lblPalabra = new JLabel("Palabra clave: ");
+		gridConst.gridx=0;
+		gridConst.gridy=5;
+		this.add(lblPalabra, gridConst);
+		
+		lblFecha = new JLabel("Fecha publicacion: ");
+		gridConst.gridx=0;
+		gridConst.gridy=6;
+		this.add(lblFecha, gridConst);
+		
+		///contenedores
+		
+		txtTitulo = new JTextField();
+		txtTitulo.setColumns(10);
+		gridConst.gridx=1;
+		gridConst.gridy=0;
+		this.add(txtTitulo, gridConst);
+		
+		txtAutor = new JTextField();
+		txtAutor.setColumns(10);
+		gridConst.gridx=1;
+		gridConst.gridy=1;
+		this.add(txtAutor, gridConst);
+
+		txtSeccion = new JTextField();
+		txtSeccion.setColumns(10);
+		gridConst.gridx=1;
+		gridConst.gridy=2;
+		this.add(txtSeccion, gridConst);
+		
+		txtCapitulo = new JTextField();
+		txtCapitulo.setColumns(10);
+		gridConst.gridx=1;
+		gridConst.gridy=3;
+		this.add(txtCapitulo, gridConst);
+		
+		txtEditorial = new JTextField();
+		txtEditorial.setColumns(10);
+		gridConst.gridx=1;
+		gridConst.gridy=4;
+		this.add(txtEditorial, gridConst);
+		
+		txtPalabra = new JTextField();
+		txtPalabra.setColumns(10);
+		gridConst.gridx=1;
+		gridConst.gridy=5;
+		this.add(txtPalabra, gridConst);
+		
+		txtFecha = new JTextField();
+		txtFecha.setColumns(10);
+		gridConst.gridx=1;
+		gridConst.gridy=6;
+		this.add(txtFecha, gridConst);
+		
+		btnFiltrar = new JButton("Filtrar");
+		btnFiltrar.addActionListener( e ->{
+			try {
+				iniciarFiltro();
+			}catch(Exception ex) {
+//			    JOptionPane.showMessageDialog(this, ex.getMessage(), "Datos incorrectos", JOptionPane.ERROR_MESSAGE);
+			}
+		});
+		gridConst.gridy=8;
+		gridConst.gridx=2;
+		this.add(btnFiltrar, gridConst);
+		
 				
 		tabla = new JTable(this.tableModel);
 		tabla.setFillsViewportHeight(true);
 		scrollPane= new JScrollPane(tabla);
 		
-		gridConst.gridx=0;
-		gridConst.gridwidth=7;	
-		gridConst.gridy=2;
+		gridConst.gridx=4;
+		gridConst.gridy=0;
+		gridConst.gridwidth=4;
+		gridConst.gridheight=9;
 		gridConst.weighty=1.0;
 		gridConst.weightx=1.0;
 		gridConst.fill=GridBagConstraints.BOTH;
@@ -59,5 +181,24 @@ public class verDocPanel extends JPanel{
 		if(actualizar) this.tableModel.fireTableDataChanged();
 	}
 
+	public void iniciarFiltro(){
+		String titulo=null;
+		String autor=null;
+		String seccion=null;
+		String capitulo=null;
+		String editorial=null;
+		String palabra=null;
+		String fecha=null;
+		
+		if(!txtTitulo.getText().isEmpty()) titulo=txtTitulo.getText();
+		if(!txtAutor.getText().isEmpty()) autor=txtAutor.getText();
+		if(!txtSeccion.getText().isEmpty()) seccion=txtSeccion.getText();
+		if(!txtCapitulo.getText().isEmpty()) capitulo=txtCapitulo.getText();
+		if(!txtEditorial.getText().isEmpty()) editorial=txtEditorial.getText();
+		if(!txtPalabra.getText().isEmpty()) palabra=txtPalabra.getText();
+		if(!txtFecha.getText().isEmpty()) fecha=txtFecha.getText();
+		
+		controller.buscar(titulo, autor, seccion, capitulo, editorial, palabra, fecha);
+	}
 	
 }
